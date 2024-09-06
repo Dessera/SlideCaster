dc8d94bb5f92beefada964a46b1dd45510d2ebda
Dessera <1533653159@qq.com>
2024-09-06 23:18:13 +0800

    chore: 更新环境文件

--- gesture_recognition/flake.nix
+++ gesture_recognition/flake.nix
@@ -1,45 +1,53 @@
 {
-  description = "SlideCaster controller driven by gesture recognition";
-
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs =
-    inputs@{ self
-    , nixpkgs
-    , flake-parts
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      # dev environment only
-      # actual deployment is ubuntu arm64
-      systems = [ "x86_64-linux" ];
+  outputs = {
+    self,
+    dream2nix,
+    nixpkgs,
+  }:
+  let
+      eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = eachSystem (system: {
+      default = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./default.nix
+          {
+            paths.projectRoot = ./.;
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+    });
+    devShells = eachSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      my_tool = self.packages.${system}.default;
+      python = my_tool.config.deps.python;
+    in {
+      default = pkgs.mkShell {
+        inputsFrom = [my_tool.devShell];
+        packages = [
+          python.pkgs.python-lsp-server
+          python.pkgs.python-lsp-ruff
+          python.pkgs.pylsp-mypy
+          python.pkgs.ipython
 
-      # Development shell
-      perSystem = { config, pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            venvDir = "venv";
-            packages = with pkgs; [
-              python312
-              poetry
-              black
-
-              nil
-              nixpkgs-fmt
-            ] ++ (with pkgs.python312Packages; [
-              pip
-              venvShellHook
-            ]);
-
-            # envs for mediapipe
-            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib:${pkgs.glib.out}/lib";
-          };
-        };
-    };
+          pkgs.ruff
+          pkgs.black
+        ];
+      };
+    });
+  };
 }
