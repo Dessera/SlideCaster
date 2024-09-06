7793451e859d8d8b4f82f69378f80a6c6d552e16
Dessera <1533653159@qq.com>
2024-09-06 13:57:29 +0800

    chore: 更新控制中心程序

---
+++ control_center/flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+
+    neovim.url = "github:Dessera/neovim";
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    nixpkgs,
+    neovim
+  }:
+  let
+      eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = eachSystem (system: {
+      default = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./default.nix
+          {
+            paths.projectRoot = ./.;
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+    });
+    devShells = eachSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      my_tool = self.packages.${system}.default;
+      neovim' = neovim.packages.${system}.default;
+      python = my_tool.config.deps.python;
+    in {
+      default = pkgs.mkShell {
+        inputsFrom = [my_tool.devShell];
+        packages = [
+          python.pkgs.python-lsp-server
+          python.pkgs.python-lsp-ruff
+          python.pkgs.pylsp-mypy
+          python.pkgs.ipython
+
+          pkgs.ruff
+          pkgs.black
+
+          neovim'
+        ];
+      };
+    });
+  };
+}
--- pdf_viewer/app.vue
+++ pdf_viewer/app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { VuePDF, usePDF } from "@tato30/vue-pdf";
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
