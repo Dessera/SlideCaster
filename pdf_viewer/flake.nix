d6d440146e23db0df6339f6f229489667e85a701
Dessera <1533653159@qq.com>
2024-09-06 12:25:37 +0800

    chore: 移除打包工具（Nix）

--- pdf_viewer/flake.nix
+++ pdf_viewer/flake.nix
@@ -1,50 +1,25 @@
 {
-  # This example flake.nix is pretty generic and the same for all
-  # examples, except when they define devShells or extra packages.
-  description = "Dream2nix example flake";
+  description = "A Nix-flake-based Node.js development environment";
 
-  # We import the latest commit of dream2nix main branch and instruct nix to
-  # re-use the nixpkgs revision referenced by dream2nix.
-  # This is what we test in CI with, but you can generally refer to any
-  # recent nixpkgs commit here.
-  inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.follows = "dream2nix/nixpkgs";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-  }: let
-    # A helper that helps us define the attributes below for
-    # all systems we care about.
-    eachSystem = nixpkgs.lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-  in {
-    packages = eachSystem (system: {
-      # For each system, we define our default package
-      # by passing in our desired nixpkgs revision plus
-      # any dream2nix modules needed by it.
-      default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./default.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
       };
-    });
-  };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm yarn ];
+        };
+      });
+    };
 }
